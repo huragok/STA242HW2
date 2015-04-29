3e69e053ecf0f5b3af5943e70426947d2ac691de
wenhao.wu@ucdavis <wnhwu@ucdavis.edu>
2015-04-29 00:50:48 -0700

    BMLGrid package body almost completed.

---
+++ BMLGrid/R/BMLGrid.R
@@ -0,0 +1,106 @@
+#' @export
+BMLGrid <- function(r, c, ncars) {
+  cars <- sample(1 : (r * c), ncars['red'] + ncars['blue']) # The vector index of the cars in the grid
+  red <- sample(cars, ncars['red'])  # The vector index of the red cars in the grid
+  blue <- setdiff(cars, red)  # The vector index of the blue cars in the grid
+  grid <- matrix(0, nrow = r, ncol = c) # The matrix representing the cars, 0 indicates no cars on that grid
+  grid[red] <- 1 # 1 indicates a red car on the grid
+  grid[blue] <- 2 # 2 indicates a blue car on the grid
+  
+  instance_BMLGrid <- list(grid = grid, red = red, blue = blue)
+  class(instance_BMLGrid) <- 'BMLGrid'
+  return(instance_BMLGrid)
+}
+
+#' @export
+plot.BMLGrid <- function(g, ...) {
+  colormap <- c("white", "red", "blue")
+  image(1 : ncol(g$grid), 1 : nrow(g$grid), t(g$grid), col = colormap, xlab = '', ylab = '')
+}
+
+#' @export
+summary.BMLGrid <- function(g, ...) {
+  lines <- c("BMLGrid class object.", paste(c(" -", toString(nrow(g$grid)), 'rows,', toString(ncol(g$grid)), 'columns'), collapse = ' '), paste(c(" -", toString(length(g$red)), 'red,', toString(length(g$blue)), 'blue.\n'), collapse = ' '))
+  return(cat(paste(lines, collapse = '\n')))
+}
+
+#' @export
+createBMLGrid <- function(r, c, ncars) {
+  return(BMLGrid(r, c, ncars))
+}
+
+#' @export
+runBMLGrid <- function(g, numSteps, movieName = NULL, recordSpeed = FALSE) {
+  r <- nrow(g$grid)
+  c <- ncol(g$grid)
+  
+  flag_movie <- !is.null(movieName)
+  if (flag_movie){
+    par(bg = "white") # ensure the background color is white
+    plot(c, r, type = "n")
+    animation::ani.record(reset = TRUE) # clear history before recording
+    plot(g) # Plot the initial g
+    animation::ani.record() # record the current frame
+  }
+  
+  if (recordSpeed) {
+    nmoved <- rep(0, numSteps + 1) # Record the number of cars moved at each step
+    nmoved[1] <- get_nmoved(g$grid, r, c, g$blue, 'up') 
+  }
+  for (step in seq_len(numSteps)) {
+    if (step %% 2 == 0) { # Red cars move to right by 1 grid
+      red_right <- idx_right(g$red, r, c) # The vector index of the right grids to current red cars
+      red_new <- ifelse(g$grid[red_right] == 0, red_right, g$red) # If not occupied, move to right. Else stay at the current grid
+      
+      g$grid[g$red] <- 0 # Update grid
+      g$red <- red_new
+      g$grid[g$red] <- 1
+      if (recordSpeed) {
+        nmoved[step + 1] <- get_nmoved(g$grid, r, c, g$blue, 'up')  # Record the number of cars moved at each step
+      }
+    } else { # Blue cars move upward by 1 grid
+      blue_up <- idx_up(g$blue, r, c) # The vector index of the right grids to current red cars
+      blue_new <- ifelse(g$grid[blue_up] == 0, blue_up, g$blue) # If not occupied, move to right. Else stay at the current grid
+      
+      g$grid[g$blue] <- 0 # Update grid
+      g$blue <- blue_new
+      g$grid[g$blue] <- 2
+      if (recordSpeed) {
+        nmoved[step + 1] <- get_nmoved(g$grid, r, c, g$red, 'right')  # Record the number of cars moved at each step
+      }
+    }
+    if (flag_movie){
+      plot(g) # Plot g
+      animation::ani.record() # record the current frame
+    }
+  }
+  if (flag_movie){
+    oopts = animation::ani.options(interval = 1)
+    animation::saveHTML(animation::ani.replay(), img.name = toString(movieName)) # export the animation to an HTML page
+  }
+  if (recordSpeed) {
+    n_moved_blue <- nmoved[seq(1, numSteps, by=2)]
+    n_moved_red <- nmoved[seq(2, numSteps, by=2)]
+    return(list(g = g, v.blue = n_moved_blue / length(g$blue), v.red = n_moved_red / length(g$red)))
+  }
+  return(g)
+}
+
+# Vectorized function to get the vector index of the right grid right to the current grid
+idx_right <- function(idx, r, c) {
+  return((idx + r - 1) %% (r * c) + 1)
+}
+
+# Vectorized function to get the vector index of the right grid right to the current grid
+idx_up <- function(idx, r, c) {
+  return(idx %% r + 1 + ((idx - 1) %/% r) * r)
+}
+
+# Function to compute the number of cars that moved given the index of cars we would like to move, the current grid and whether we would like to move up or right
+get_nmoved <- function(grid, r, c, cars, direction) {
+  if (direction == 'up') {
+    return(sum(grid[idx_up(cars, r, c)] == 0))
+  } else {
+    return(sum(grid[idx_right(cars, r, c)] == 0))
+  }
+}
